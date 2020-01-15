9bfb5cac7ab749fb52054f025e41abfcc9bd16ce
andreygnuchikh <44124200+andreygnuchikh@users.noreply.github.com>
2020-01-16 01:12:26 +0300

    add new MainPage and write Main method

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,8 @@
+from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.find_element_by_css_selector("#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
